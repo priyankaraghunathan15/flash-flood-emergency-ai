d54b9462f330d44d82a7bfa0e4a09b51d6a3d75f
Priyanka Raghunathan <priyanka.ragz@gmail.com>
2025-11-02 10:23:20 -0500

    Update api-proxy.js

--- flood-dashboard/netlify/functions/api-proxy.js
+++ flood-dashboard/netlify/functions/api-proxy.js
@@ -1,31 +1,38 @@
+const axios = require('axios');
+
 exports.handler = async (event) => {
   const API_BASE = 'http://45.33.73.99:5000/api';
   
-  // Get the path and method from the request
+  // Get the path after /api-proxy
   const path = event.path.replace('/.netlify/functions/api-proxy', '');
   const url = `${API_BASE}${path}`;
   
   try {
-    const response = await fetch(url, {
+    const response = await axios({
       method: event.httpMethod,
-      headers: event.headers,
-      body: event.body
+      url: url,
+      data: event.body ? JSON.parse(event.body) : undefined,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
     
-    const data = await response.text();
-    
     return {
-      statusCode: response.status,
-      body: data,
+      statusCode: 200,
+      body: JSON.stringify(response.data),
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*'
       }
     };
   } catch (error) {
+    console.error('Proxy error:', error.message);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({ 
+        error: error.message,
+        url: url 
+      })
     };
   }
 };
